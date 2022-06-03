f2c23ec5808048f9b049b508c272f122783b6d6b
Ebrahim Kareem <ebrahemkreem40@gmail.com>
2022-06-03 22:07:23 +0300

    Updates

---
+++ precache-manifest.9c3ca3d68fae5aa7349d23e3bd867dd6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a05256a7fbea01db8dd",
+    "url": "/ebrahim-2/simple-lookup/static/js/main.8a05256a.chunk.js"
+  },
+  {
+    "revision": "588dd365cc8981181848",
+    "url": "/ebrahim-2/simple-lookup/static/js/1.588dd365.chunk.js"
+  },
+  {
+    "revision": "71ee291529bb357002b4",
+    "url": "/ebrahim-2/simple-lookup/static/css/2.1227a6b6.chunk.css"
+  },
+  {
+    "revision": "71ee291529bb357002b4",
+    "url": "/ebrahim-2/simple-lookup/static/js/2.71ee2915.chunk.js"
+  },
+  {
+    "revision": "e02698887680ecdd2baa",
+    "url": "/ebrahim-2/simple-lookup/static/js/runtime~main.e0269888.js"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ebrahim-2/simple-lookup/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ebrahim-2/simple-lookup/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ebrahim-2/simple-lookup/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ebrahim-2/simple-lookup/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ebrahim-2/simple-lookup/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ebrahim-2/simple-lookup/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ebrahim-2/simple-lookup/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ebrahim-2/simple-lookup/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ebrahim-2/simple-lookup/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ebrahim-2/simple-lookup/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ebrahim-2/simple-lookup/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ebrahim-2/simple-lookup/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ebrahim-2/simple-lookup/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ebrahim-2/simple-lookup/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ebrahim-2/simple-lookup/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ebrahim-2/simple-lookup/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "d74ab2f68d650634c9e835c578f98eef",
+    "url": "/ebrahim-2/simple-lookup/index.html"
+  }
+];
